44af51369c54e186c3d4f04487a177a474b90710
sarycrp <149130436+sarycrp@users.noreply.github.com>
2023-10-26 23:08:22 -0400

    Tarea 6 Couteau

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tarea_6',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
